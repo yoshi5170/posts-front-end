aade4b72887be0226579cd172d4b96e0737bf9f6
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2024-04-29 23:03:44 +0900

    practice

---
+++ src/PostCreate.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+// import {postPost} from "./api"
+function PostCreate(){
+  const [title, setTitle] = useState('');
+  const navigateToPosts = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newPost = {
+      title: title,
+    }
+
+    await postPostFunc(newPost)
+
+    setTitle('');
+  }
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  }
+
+  const postPostFunc = async (post) => {
+    try {
+      await postPost(post)
+      navigateToPosts("/")
+    }catch (error) {
+      console.log(error);
+    }
+  };
+
+  const postPost = async (params) =>
+  axios.post("/posts", (params));
+
+  return (
+    <div>
+      <h2>Post Create</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="title" />
+            <input type="text" value={title} onChange={handleTitleChange} id="title" required/>
+          </div>
+          <button type="submit">作成</button>
+        </form>
+    </div>
+  )
+}
+
+export default PostCreate;

67ef7853cb5a1daf467f86ac0616a23c2b34f365
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2024-04-30 23:02:58 +0900

    practice

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import './App.css';
+import axios from 'axios';
+import Posts from './Posts';
+import PostCreate from './PostCreate';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+
+// const API_URL = "http://localhost:3000/api/v1/posts";
+
+function App() {
+  // const [posts, setPosts]=useState([]);
+  // const getAPIData = async () => {
+  //   try {
+  //     const res = await axios.get(API_URL)
+  //     const postData = res.data.data
+  //     setPosts(postData);
+  //   }catch(error){
+  //     console.error('Error fetching data:', error);
+  //   }
+  // };
+  // useEffect(()=>{
+  //   getAPIData();
+  // }, []);
+
+
+  return (
+    <Router>
+      <Box sx={{ flexGrow: 1}}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" component="div">
+              ブログ
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Routes>
+        <Route path="/" element={<Posts />} />
+        <Route
+          path="/posts/create"
+          element={<PostCreate/>}
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;

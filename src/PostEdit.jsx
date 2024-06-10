7b4df1b1b23f22fddfead9615c3ae7010f8327c1
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2024-06-10 20:24:07 +0900

    aff post edit file

---
+++ src/PostEdit.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import {getPost, updatePost} from "./infra/api";
+
+function PostEdit(){
+  return(
+    
+  )
+}
+export default PostEdit;

3fd5e933d1724e70703d47f0e33f10a74d865849
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2024-06-26 20:44:07 +0900

    レスポンシブ対応に修正

--- src/Top.jsx
+++ src/Top.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
 function Top({ posts }) {
-  if (posts.length === 0) {
-    return null;
-  }
-
   return (
     <div className="container sm:ml-64">
-      <p className="m-4 text-gray-500">サイドバーから詳細を確認できます</p>
+      <p className="m-4 text-gray-500 sm:hidden">メニューバーから詳細を確認できます</p>
+      {posts.length > 0 && (
+        <p className="m-4 text-gray-500 hidden sm:block">サイドバーから詳細を確認できます</p>
+      )}
     </div>
   );
 }

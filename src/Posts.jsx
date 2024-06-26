e7550f30c784c557d9eaee8d235492be4af86de7
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2024-06-26 13:37:39 +0900

    エラーの処理を追加

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { getPosts, deletePost } from "./infra/api";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from 'react';
-import ListItemText from '@mui/material/ListItemText';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 
@@ -10,15 +9,21 @@ import DeleteIcon from '@mui/icons-material/Delete';
 function Posts({posts, setPosts}){
   const navigate = useNavigate();
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   const getAPIData = async () => {
     try {
       const res = await getPosts();
       const postData = res.data.data;
       setPosts(postData);
+      setLoading(false);
     }catch(error){
-      console.error('Error fetching data:', error);
+      console.error('Error deleting post:', error);
+      setError("An unexpected error occurred.");
+      setLoading(false);
     }
   };
+
   useEffect(()=>{
     getAPIData();
   }, []);
@@ -28,35 +33,41 @@ function Posts({posts, setPosts}){
       try {
         await deletePost(id);
         getAPIData();
+        // setLoading(false);
+        navigate(`/`);
       } catch (error) {
-        setError(error);
+        console.error(error);
+        setError("An unexpected error occurred.");
+        setLoading(false);
       }
     }
   };
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
-  if (!posts.length) {
-    return <div>todoがありません</div>;
-  }
-
   return (
-    <aside class="fixed top-14 left-0 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 border-r-2 border-gray-200" >
-      <div class="h-full px-3 py-4 overflow-y-auto">
-        <ul className="space-y-2">
-          {posts.map((post) => (
-            <div key={post.id} className="flex items-end justify-between px-2 pt-2 text-gray-900 hover:bg-gray-100 border-b-2 border-orange-800 border-opacity-50 hover:bg-gray-200">
-              <li className="flex-grow">
-                <p onClick={() => navigate(`/posts/${post.id}`)}>{post.title}</p>
-              </li>
-              <IconButton aria-label="delete" onClick={() => handleDelete(post.id)}>
-                <DeleteIcon className="text-orange-800"/>
-              </IconButton>
-            </div>
-          ))}
-        </ul>
+    <aside className="fixed top-14 left-0 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 border-r-2 border-gray-200" >
+      <div className="h-full px-3 py-4 overflow-y-auto">
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>Error: {error}</p>
+        ) : posts.length === 0 ? (
+          <p className="text-sm text-gray-500">現在、ToDoはありません。右上の+ボタンから新しいToDoを追加できます。</p>
+        ) : (
+          <>
+            <ul className="space-y-2">
+              {posts.map((post) => (
+                <div key={post.id} className="flex items-end justify-between px-2 pt-2 text-gray-900 hover:bg-gray-100 border-b-2 border-orange-800 border-opacity-50 hover:bg-gray-200">
+                  <li className="flex-grow">
+                    <p onClick={() => navigate(`/posts/${post.id}`)}>{post.title}</p>
+                  </li>
+                  <IconButton aria-label="delete" onClick={() => handleDelete(post.id)}>
+                    <DeleteIcon className="text-orange-800"/>
+                  </IconButton>
+                </div>
+              ))}
+            </ul>
+          </>
+        )}
       </div>
     </aside>
   );
--- src/components/PostCreateModal.jsx
+++ src/components/PostCreateModal.jsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
 import { useNavigate } from "react-router-dom";
 import {postPost} from "../infra/api";
-export function PostCreateModal ({show, setShow, setPosts}) {
+function PostCreateModal ({show, setShow, setPosts}) {
   const[title, setTitle] = useState('');
-  const navigateToPosts = useNavigate();
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
   const handleSubmit = async (e) => {
     e.preventDefault();
     const newPost = {
@@ -11,20 +12,25 @@ export function PostCreateModal ({show, setShow, setPosts}) {
     };
     // const newPost = { title }; <=上記と同じ意味
     await postPostFunc(newPost)
-    setTitle('');
   }
-
 
   const postPostFunc = async (post) => {
     try {
       const res = await postPost(post)
-      const newPost = res.data.data;
-      setShow(false)
-      navigateToPosts("/")
-      addPost(newPost);
-
-    }catch (error) {
-      console.log(error);
+      if (res.data.status === "ERROR") {
+        const errorMessage = res.data.data.title ? res.data.data.title[0] : "An unexpected error occurred.";
+        setError(errorMessage);
+      } else {
+        const newPost = res.data.data;
+        setShow(false);
+        setTitle('');
+        setError(null);
+        navigate("/");
+        addPost(newPost);
+      }
+    } catch (error) {
+      console.error(error);
+      setError("An unexpected error occurred.");
     }
   };
 
@@ -39,19 +45,28 @@ export function PostCreateModal ({show, setShow, setPosts}) {
   if(show) {
     return(
       <div className="fixed top-0 left-0 w-full h-full bg-slate-900 bg-opacity-75 z-10">
-        <div className="mx-auto mt-60 p-2 w-1/4 z-20 bg-white text-black text-center rounded-lg">
-          <p className="text-left px-2 font-bold">リストを作成</p>
+        <div className="mx-auto mt-60 p-2 w-1/3 z-20 bg-white text-center rounded-lg">
+          <p className="text-left px-2 font-bold text-gray-600">リストを作成</p>
           <form className="mt-2" onSubmit={handleSubmit}>
-            <div className="flex flex-col text-left my-4 px-2 text-black w-1/2">
-              <label htmlFor="title" className="font-light text-xs">タイトル</label>
-              <input className="border-2 rounded-lg border-gray-200" type="text" value={title} onChange={handleTitleChange} id="title" required/>
+            <div className="flex space-y-4 flex-col text-left my-4 px-2 text-black w-full">
+              <label htmlFor="title" className="font-light text-gray-500 text-sm">リスト名</label>
+              <input
+              className="w-full border-b-2 border-gray-400 text-sm pl-2"
+              type="text"
+              value={title}
+              onChange={handleTitleChange}
+              id="title"
+              placeholder="create a new list"
+              required
+              />
+              {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
             </div>
             <div className="flex justify-end mx-2 space-x-2">
               <div>
-                <button className="rounded-lg hover:bg-gray-300 px-2 text-sm" type="submit">作成</button>
+                <button className="rounded text-gray-600 hover:bg-gray-300 p-2 text-sm" type="submit">作成</button>
               </div>
               <div>
-                <button className="rounded-lg hover:bg-gray-300 px-2 text-sm" onClick={()=> setShow(false)}>キャンセル</button>
+                <button className="rounded text-gray-600 hover:bg-gray-300 p-2 text-sm" onClick={()=> setShow(false)}>キャンセル</button>
               </div>
             </div>
           </form>
@@ -61,4 +76,6 @@ export function PostCreateModal ({show, setShow, setPosts}) {
   }else{
     return null;
   }
-}
+};
+
+export default PostCreateModal;
